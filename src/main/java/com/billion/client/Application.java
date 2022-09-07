af22c7c71ba088c841e7c4775bc581c0f635980f
leeqiang250 <leeqiang250@163.com>
2022-09-07 19:46:35 +0800

    no message

--- src/main/java/com/billion/client/Application.java
+++ src/main/java/com/billion/client/Application.java
@@ -19,19 +19,11 @@ public class Application {
 	}
 
 	@Value("${demo.param1}")
-	private String param1;
+	private String param;
 
-	@Value("${demo.param2}")
-	private String param2;
-
-	@RequestMapping(value = "/demo")
+	@RequestMapping(value = {"/", "/demo"})
 	public String hi() {
-		return param1 + " and " + param2;
-	}
-
-	@RequestMapping(value = "/")
-	public String hiss() {
-		return param1 + " 1and " + param2;
+		return param;
 	}
 
 }
